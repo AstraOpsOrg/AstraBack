74914c1f356faf451deeaff7662f506ef96134ca
Jorge Florentino <jorgeflorentinomarbe@gmail.com>
2025-05-26 20:33:08 +0200

    introducing deploy with cloudfare workers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-// index.ts
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Bun and Hono!')
+  return c.text('Hello Hono!')
 })
 
 export default app
