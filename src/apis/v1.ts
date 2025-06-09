735893194d3ea838d1296def03f4755baff6e858
Jorge Florentino <jorgeflorentinomarbe@gmail.com>
2025-06-09 20:14:11 +0200

    fixes

--- src/apis/v1.ts
+++ src/apis/v1.ts
@@ -1,7 +1,12 @@
 import { Hono } from "hono"
+const basePath = '/v1'
 
 function loadV1(app: Hono) {
-  app.get('/', (c) => {
+  app.get(`${basePath}`, (c) => {
+    return c.json({ message: 'AstraOps API v1' })
+  })
+
+  app.get(`${basePath}/platform`, (c) => {
     return c.json({ message: 'AstraOps API v1' })
   })
 }
